4b0418e563388323e22250dd03f5f7e36200baf2
shaunvan119 <shaunvan@live.com.au>
2022-06-07 15:05:58 +1000

    updating product data

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,26 +1,45 @@
+import productsData from "./productsData"
+
+
 function MainContent() {
+    const products = productsData.map(prod => (
+        <div className="grid-container" key={prod.id}>
+            <h3>{prod.title}</h3>
+            <p>{prod.description}</p>
+        </div>
+    ))
     
     return (
         <div className="main-container">
-            <div className="grid-container-one">
-
-            </div>
-            <div className="grid-container">
-
-            </div>
-            <div className="grid-container">
-
-            </div>
-            <div className="grid-container">
-
-            </div>
-            <div className="grid-container">
-
-            </div>
-            <div className="grid-container">
-
-            </div>
+        <div className="grid-container-one">
         </div>
+        {products}
+        </div>
+
+
+        /*<>
+            
+            {products}
+        </>*/
+        /*<div className="main-container">
+            <div className="grid-container-one">
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+        </div>*/
     )
     
 }
---
+++ src/components/ProductsData.js
@@ -0,0 +1,32 @@
+export default  [
+    {
+        id: 1,
+        title: "Cold calling",
+        description: "Everyone's favorite cat who loves to hate",
+        
+    },
+    {
+        id: 2,
+        title: "Stretch Armstrong",
+        description: "He bends! He stretches! He even ties in knots, but always returns to his original shape!",
+        
+    },
+    {
+        id: 3,
+        title: "Hungry Hungry Hippos Game",
+        description: "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
+        
+    },
+    {
+        id: 4,
+        title: "Crossfire board game",
+        description: "You'll get caught up in the crossfire!",
+        
+    },
+    {
+        id: 4,
+        title: "Crossfire board game",
+        description: "You'll get caught up in the crossfire!",
+        
+    }
+]
