a4fc35d1de2695929a8a30e20ab70c229691ccb9
shaunvan119 <shaunvan@live.com.au>
2022-06-07 14:12:15 +1000

    main section draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import Header from "./components/Header";
 import TopSection from "./components/TopSection";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
       <TopSection/>
+      <MainContent/>
     </div>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,28 @@
+function MainContent() {
+    
+    return (
+        <div className="main-container">
+            <div className="grid-container-one">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+            <div className="grid-container">
+
+            </div>
+        </div>
+    )
+    
+}
+
+export default MainContent
