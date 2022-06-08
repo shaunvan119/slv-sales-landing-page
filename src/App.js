7b3fa7c4349ce96f37a686d55bcf3d4eb6332a62
shaunvan119 <shaunvan@live.com.au>
2022-06-08 19:20:20 +1000

    added price component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from "./components/Header";
 import TopSection from "./components/TopSection";
 import MainContent from "./components/MainContent";
 import Contact from "./components/Contact";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
     <div className="wrapper">
 
       <Routes>
-      <Route path="/" element={[<Header/>,<TopSection/>,<MainContent/>]}/>   
+      <Route path="/" element={[<Header/>,<TopSection/>,<MainContent/>, <Pricing/>]}/>   
           <Route path="/contact" element={<Contact/>}/>    
       </Routes>
     </div>
