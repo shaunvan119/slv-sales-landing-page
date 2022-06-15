e90a9c2d5e53cca7134d73254027e17d084572cd
shaunvan119 <shaunvan@live.com.au>
2022-06-15 12:09:30 +1000

    updated info page components

--- src/components/infopages/AutoPage.js
+++ src/components/infopages/AutoPage.js
@@ -1,14 +1,20 @@
-
+import Header from "../Header"
 import CountAppts from "../CountAppt"
 
 function AutoPage() {
     
     return (
         <div className="top-section">
+        <Header/>
             <h2 className="top-heading">Minimize your spend<br/>on marketing and wages</h2> 
-            <p className="top-paragraph">Increase leads without the huge cost of recruiting and retaining B2B and sales reps,<br/> or spending thousands on marketing agencies with no transparency or results.<br/> 
-            my lead generation approach combines a mix of manual outbound calling, <br/>
-            automated cold outreach and building websites that can create a flood of <br/>hungry buyers.</p>
+            <p className="top-paragraph">With so many emerging software tools on the market to help automate your outbound strategy<br/> 
+                it can be confusing on what tools, messages and target audience will deliver results. <br/>
+                We will generate an audience list, create email and linkedIn messaging templates <br/>
+                and set up automated monthly campaigns that will run while you sleep. <br/>
+                Marketing companies will focus on paid adds/key words to target a broad audience, <br/>
+                our approach is to focus on a specific data list of decision makers who <br/>
+                hold positions relevant to your target audience. <br/>
+                In return lower lower cost per lead.  </p>
         
         </div>
         
--- src/components/infopages/WebCreation.js
+++ src/components/infopages/WebCreation.js
@@ -1,13 +1,20 @@
 import CountWeb from "../CountWeb"
+import Header from "../Header"
 
 function WebCreation() {
     
     return (
         <div className="top-section">
-            <h2 className="top-heading">Minimize your spend<br/>on marketing and wages</h2> 
-            <p className="top-paragraph">Increase leads without the huge cost of recruiting and retaining B2B and sales reps,<br/> or spending thousands on marketing agencies with no transparency or results.<br/> 
-            my lead generation approach combines a mix of manual outbound calling, <br/>
-            automated cold outreach and building websites that can create a flood of <br/>hungry buyers.</p>
+        <Header/>
+            <h2 className="top-heading">Fully responsive website with purpose</h2> 
+            <p className="top-paragraph">We will design a fully responsive website that will convert traffic into buyers, your website has one job, generate leads. <br/>
+            Web designers are great at making things pretty but they can not build you a website that can attract buyers, typically a web designer doesn't have<br/>
+             a background in the psychology of a consumer's buying behavior to attract buyers. Your website needs to have emotional triggers to convert <br/>
+             online traffic to hit that submit or buy button. </p>
+          <p className="top-paragraph">Your website is a central hub for all your marketing spend, you spend thousands of dollars to direct online traffic to your page<br/>
+           with no conversion, and your marketing team or agency get excited because clicks have increased, that's great but you need leads not another fluff meeting <br/>
+           with a marketing agency discussing stats around clicks.</p>
+           
         <div className="counter-section">
             <CountWeb/> 
             </div> 
