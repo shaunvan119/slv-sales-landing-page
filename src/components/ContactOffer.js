026d1abab6d9e59feefeb0c38fdc33259fb9a29b
shaunvan119 <shaunvan@live.com.au>
2022-06-28 14:02:12 +1000

    contact offer form created

---
+++ src/components/ContactOffer.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Result = () => {
+  return (
+    <p>Your message has been sent</p>
+  );
+};
+
+const ContactOffer = () => {
+  const [result, showResult] = useState(false)
+ 
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_k4aiwmr",
+        "template_rer8is9",
+        form.current,
+        "Fh0Y9YM-DloiKhGif"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+       
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      e.target.reset();
+      showResult(true)
+  };
+
+  return (
+     <div className="form-section">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <div> {result ? <Result/> : null}</div>
+        <input type="submit" value="Send" />
+      </form>
+   </div> 
+  );
+};
+
+export default ContactOffer;
--- src/components/infopages/WebCreation.js
+++ src/components/infopages/WebCreation.js
@@ -1,10 +1,12 @@
-import CountWeb from "../CountWeb"
+
 import Header from "../Header"
 import apparel from "./portfolio/baseapparel.png"
 import payapi from "./portfolio/pay.png"
 import suite from "./portfolio/suite.png"
 import bbq from "./portfolio/bbq.png"
-import { Link } from "react-router-dom"
+import webpic from "./portfolio/image-web-design-large.jpg"
+import ContactOffer from "../ContactOffer"
+
 
 
 
@@ -15,33 +17,47 @@ function WebCreation() {
         <div className="container-web">
         <Header/>
         
-        <div className="top-section">
-        
-            <h2 className="top-heading">Fully responsive website<br/>
-             with purpose</h2> 
-            <p className="top-paragraph">We will design a fully responsive website that will convert traffic into buyers,<br/> your website has one job, generate leads. 
-            Web designers are great at making<br/> 
-            things pretty but they can not build you a website that can attract buyers,<br/> 
-            typically a web designer doesn't have a background in the psychology<br/> 
-            of a consumer's buying behavior to attract buyers. Your website needs to <br/> have emotional triggers to convert online traffic to hit that<br/> 
-            submit or buy button. </p>
-          <p className="top-paragraph">Your website is a central hub for all your marketing spend, you spend thousands<br/> 
-            of dollars to direct online traffic to your page with no conversion, and your<br/>
-            marketing team or agency get excited because clicks have increased,<br/> 
-            that's great but you need leads not another fluff meeting with a <br/>
-            marketing agency discussing stats around clicks.</p>
-           <h1 className="projects-header">Some of our projects</h1>
-           <div className="portfolio-grid">
-        
+        <div className="top-section-webdesign">
+         <div className="top-section-left">
+                <h2 className="heading-webdesign">Fully responsive website<br/>
+                with purpose</h2> 
+                <p className="paragraph-webdesign">We will design a fully responsive website that will convert traffic into buyers, your website has one job, generate leads. 
+                Web designers are great at making things pretty but they can not build you a website that can attract buyers, typically a web designer doesn't have a background in the psychology of a consumer's buying behavior to attract buyers. Your website needs to <br/> have emotional triggers to convert online traffic to hit that<br/> 
+                submit or buy button. </p>
+            <p className="paragraph-webdesign">Your website is a central hub for all your marketing spend, you spend thousands
+                of dollars to direct online traffic to your page with no conversion, and your
+                marketing team or agency get excited because clicks have increased,
+                that's great but you need leads not another fluff meeting with a 
+                marketing agency discussing stats around clicks.</p>
+        </div>
+                 <div className="contactOffer">
+                 <ContactOffer/>
+         </div>
+        </div>
+        <div className="about-section">
+        <div className="about-section-left">
+            <h1 className="h1-webdesign">Turn website visitors<br/> into buyers</h1>
+            <p className="p-webdesign">
+             Get it right the first time, dont spend 30K<br/> on a landing page with all the
+             bells and whistles.<br/> From the point your prospect hits your website<br/> 
+             you have less then 15 secounds to attract<br/> their attention.</p>
+        </div>
+        <div className="computer-image">
+                <img class="portfolio-grid-images image-computer" src={webpic} alt="computer"/>
+                <div class="centered"><span className="webdesign">NEXT LEVEL</span><br/><span className="view-projects">WEB DEVELOPMENT</span></div>
+        </div>
+        </div>
+                <h1 className="projects-header">Some of our projects</h1>
+        <div className="portfolio-grid">
                 <a href="https://magnificent-dieffenbachia-b7bc19.netlify.app/" target="_blank" rel="noreferrer"><img class="portfolio-grid-images" src={apparel} alt="apparel"/></a>
                 <a href="https://symphonious-chebakia-166971.netlify.app/" target="_blank" rel="noreferrer"><img class="portfolio-grid-images" src={payapi} alt="payapi"/></a>
                 <a href="https://regal-gecko-130083.netlify.app/" target="_blank" rel="noreferrer"><img class="portfolio-grid-images" src={suite} alt="suite"/></a>
                 <a href="https://shaunvan119.github.io/bbq-webpage/" target="_blank" rel="noreferrer"><img class="portfolio-grid-images" src={bbq} alt="bbq"/></a>
-           </div>
+        </div>
               
            
             
-        </div>
+       
       </div>  
     )
 }
