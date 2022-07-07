5fbf9a3083c507742de2c210e33da06011e3a54d
shaunvan119 <shaunvan@live.com.au>
2022-07-07 15:33:28 +1000

    css web design page

--- src/components/Topsection.js
+++ src/components/Topsection.js
@@ -2,18 +2,20 @@
 import CountRev from "./CountRev"
 import CountLeads from "./CountLeads"
 import CountWeb from "./CountWeb"
+import Circle from "../components/infopages/portfolio/bg-pattern-circle.svg"
 
 
 function TopSection() {
     
     return (
-        
+        <div className="top-section-container">
         <div className="top-section">
-      
+         <div className="top-right">
             <h2 className="top-heading">Super charge your lead<br/> generation</h2> 
             <p className="top-paragraph">Increase leads without the huge cost of recruiting and retaining B2B and sales reps,<br/> or spending thousands on marketing agencies with no transparency or results.<br/> 
             our lead generation approach combines a mix of manual outbound calling, <br/>
             automated cold outreach and building websites that can create a flood of <br/>hungry buyers.</p>
+         </div>
         <div className="counter-section">
             <CountRev/>
             <CountLeads/>
@@ -21,7 +23,8 @@ function TopSection() {
             </div> 
             <div className="color-item"></div>
         </div>
-        
+
+       </div> 
     )
 }
 
